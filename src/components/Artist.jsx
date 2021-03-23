ba998a69207f3a6c3f83c8a061bad4c890c8a068
bhead4 <gooingyu@outlook.com>
2021-03-23 08:58:56 +0800

    feat(ready)

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { replaceSmallImg } from 'utils/index';
-import classes from '../card.css';
+import { replaceSmallImg } from '@utils/index';
+import classes from './Card/card.css';
 
 export default function Artist({ id, name, avatar }) {
         return (
           <div
-            className={classes.avatarLine} 
+            className={classes.avatarLine}
             onClick={() => window.open(`https://www.sharemoe.net/artist/${id}`)}>
             <div
               style={{ backgroundImage: `url(${replaceSmallImg(avatar)})` }}
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
-import { replaceSmallImg } from 'utils/index';
+import { replaceSmallImg } from '../../utils/index';
 import classes from './card.css';
 
 export default function Card({ imagesUrl }) {
